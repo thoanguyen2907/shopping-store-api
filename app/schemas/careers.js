d7f5d4fd56625664a896f6e2c04726d447e7fb3f
thoanguyen2907 <thoanguyen290795@gmail.com>
2021-10-10 13:23:51 +0300

    create relationship between restaurannt and careers table

--- app/schemas/careers.js
+++ app/schemas/careers.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
 const databaseConfig = require("../configs/database");
+const {Schema} = require('mongoose');
 
 var schema = new mongoose.Schema({ 
 	_id: String,
     name 		    : String,
 	title 			: String,
 	like		    : Number,
-	dislike			: Number
+	dislike			: Number,
+	
 });
+schema.virtual('restaurants', {
+	ref: 'items', //The Model to use
+	localField: '_id', //Find in Model, where localField 
+	foreignField: 'careers', // is equal to foreignField
+ });
+
+ // Set Object and Json property to true. Default is set to false
+ schema.set('toObject', { virtuals: true });
+ schema.set('toJSON', { virtuals: true });
 
 module.exports = mongoose.model(databaseConfig.col_careers, schema );
--- app/schemas/items.js
+++ app/schemas/items.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
 const databaseConfig = require("../configs/database");
-
+const {Schema} = require('mongoose');
 var schema = new mongoose.Schema({ 
     name 		    : String,
 	description 	: String,
-	careers	        : [String],
+	careers	        : [{
+		type: Schema.Types.ObjectId,
+		ref: 'careers',
+		required: true
+	 }],
 	type		    : [String],
 	local		    : [String],
 	web		        : String,
 	address 	    : String,
 	phone		    : Number,
-	email		    : String
+	email		    : String, 
+	
 });
 
 module.exports = mongoose.model(databaseConfig.col_items, schema );
